93413bd6813a47d4593edfa654c7908cea9ffc53
abellizzistudent <108248109+abellizzistudent@users.noreply.github.com>
2023-06-13 20:54:41 -0700

    Update dbPool.js
    Removed plain text DB password

--- dbPool.js
+++ dbPool.js
@@ -4,7 +4,7 @@ const pool  = mysql.createPool({
     connectionLimit: 10,
     host: "lcpbq9az4jklobvq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     user: "gnfgzzid9inrpez5",
-    password: "u418rshfveix6k1g",
+    password: "   ",
     database: "qo3892akxcremf7q"
 });
 
